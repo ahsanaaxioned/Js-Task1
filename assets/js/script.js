e6700a5c7912081ecef46903986918311e964598
ahsanaaxioned <ahsana@axioned.com>
2022-10-12 10:40:35 +0530

    js corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,8 @@ let a = 0;
 // working on image changes start here
 setInterval(function () {
     img.src = images[a];
-    // console.log(a);
     a++;
     if (a >= images.length) {
-        images.unshift("https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80")
-        // console.log(images);
         a = 0;
     };
 }, 1000);

9c0d0ae118a9dc5ab90ff12873291e6cb6ec59fd
ahsanaaxioned <ahsana@axioned.com>
2022-10-12 11:06:55 +0530

    changes in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let img = document.querySelector(".image");
 let images = [
     "https://media.istockphoto.com/photos/day-at-the-office-picture-id1390307383?s=612x612",
     "https://media.istockphoto.com/photos/japanese-woman-helping-her-colleague-picture-id1366415101?s=612x612",
-    "https://media.istockphoto.com/photos/digitally-enhanced-shot-of-two-handsome-businessmen-working-in-the-picture-id1322205575",
+    "https://media.istockphoto.com/photos/young-businesswoman-calculating-bill-in-office-picture-id1042650228?s=612x612",
     "https://media.istockphoto.com/photos/close-up-of-a-smart-young-woman-coding-picture-id1332378618?s=612x612",
     "https://media.istockphoto.com/photos/teacher-writing-on-the-whiteboard-in-the-classroom-picture-id1350900187?s=612x612"
 ];
@@ -18,3 +18,5 @@ setInterval(function () {
     };
 }, 1000);
 // working on image changes end here
+
+
